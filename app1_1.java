5432036097c6593dc249227707fda28ad3d9a042
wuling31715 <wuling31715@gmail.com>
2019-03-10 22:43:57 +0800

    init

---
+++ app1_1.java
@@ -0,0 +1,11 @@
+public class app1_1
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello Java!!");
+    }
+}
+
+/* app1_1 OUTPUT------------
+Hello Java!!
+---------------------------*/
