e3bdaa887cd329324169412056e358517c58de0b
wuling31715 <wuling31715@gmail.com>
2019-03-14 19:04:15 +0800

    ch02

---
+++ args/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int length = args.length;
+        for (int i = 0; i < length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
