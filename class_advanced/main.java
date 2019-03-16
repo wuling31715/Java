34969316d6526e3e583144ec5d9024537055de04
wuling31715 <wuling31715@gmail.com>
2019-03-16 14:50:38 +0800

    class

--- class_advanced/main.java
+++ class_advanced/main.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 class Calculator {
 
     float x, y;
+    // class variable
+    static float z;    
+
+    // constructor    
+    public Calculator(float x, float y){ 
+        this.x = x;
+        this.y = y;
+    }
 
     float add(){
         return this.x + this.y;
@@ -20,6 +28,16 @@ class Calculator {
         return this.x / this.y;
     }
 
+    float sum(){
+        this.z++;
+        return this.z;
+    }
+
+    // class method
+    static void class_method(){
+        System.out.println("class method.");
+    }
+
 }
 
 public class main {
@@ -27,9 +45,10 @@ public class main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        Calculator calculator = new Calculator();
+        float x, y;
         Boolean repeat = true;
         String option;
+        Calculator calculator = new Calculator(0, 0);
 
         while (repeat) {
 
@@ -39,8 +58,11 @@ public class main {
             option = scanner.next();
             System.out.print("y = ");
             calculator.y = scanner.nextFloat();
-
+            
             switch (option) {
+                case "0":
+                    repeat = false;
+                    break;
                 case "+":
                     System.out.println(calculator.add());                
                     repeat = false;
@@ -57,11 +79,22 @@ public class main {
                     System.out.println(calculator.div());
                     repeat = false;
                     break;
+                case "s":
+                    System.out.println(calculator.sum());
+                    break;
                 default:
                     System.out.println("try again.");
                     repeat = true;
                     break;
             }
         }
+        Calculator calculator2 = new Calculator(0, 0);
+        System.out.println(calculator2.sum());
+        Calculator.class_method();
+        // garbage collection
+        calculator = null;
+        System.out.println(calculator.sum());
+        System.gc();
+        System.out.println(calculator2.sum());
     }
 }
