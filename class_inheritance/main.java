ddffce52cbe817e3f9ee069dd0b43bc873fb85d4
wuling31715 <wuling31715@gmail.com>
2019-03-16 15:18:30 +0800

    class

--- class_inheritance/main.java
+++ class_inheritance/main.java
@@ -1,10 +1,31 @@
+class SuperClass {
+
+    public int x = 0;
+    private int y = 0;
+    protected int z = 0;
+
+    void show(){
+        System.out.println(this.x);
+        System.out.println(this.y);
+        System.out.println(this.z);
+        System.out.println();
+    }
+}
+
+class SubClass extends SuperClass {
+    
+}
+
+
 public class main
 {
     public static void main(String args[])
     {
-        System.out.println("Hello World!");
+        SubClass subclass = new SubClass();
+        subclass.show();
+        subclass.x = 1;
+        // subclass.y = 1;    
+        subclass.z = 1;
+        subclass.show();        
     }
 }
-
-// javac main.java
-// java main
