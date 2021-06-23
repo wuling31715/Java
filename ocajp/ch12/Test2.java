1670202792c5837198678a3b26d7c94f99bd3fb4
wuling31715 <wuling31715@gmail.com>
2021-06-23 22:01:14 +0800

    .

--- ocajp/ch12/Test2.java
+++
@@ -1,8 +0,0 @@
-class Test2{
-    public static void main(String[] args){
-        Short s1 = 11;
-        Integer s2 = 21;
-        Long s3 = (long) s1 + s2;
-        // String s4 = (String) (s2 + s3); False
-    }
-}
--- ocpjp/args/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        int length = args.length;
-        for (int i = 0; i < length; i++) {
-            System.out.println(args[i]);
-        }
-    }
-}
